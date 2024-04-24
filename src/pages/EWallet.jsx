0d824374a1dfc53ff1dd04c04e70be2587738dad
Andreas Bergström <andreas@textalkgroup.com>
2024-04-24 12:56:02 +0200

    Tweaking card component

--- src/pages/EWallet.jsx
+++ src/pages/EWallet.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Top from "../components/Top";
+import Card from "../components/Card";
 
 const EWallet = () => {
-  return <div>Start</div>;
+  return (
+    <>
+      <Top title={"add a new bank card"} />
+      <Card />
+    </>
+  );
 };
 
 export default EWallet;

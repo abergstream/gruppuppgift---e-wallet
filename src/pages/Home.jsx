032f3018971624b50435be3a16f1b1d56c01d4ae
Andreas Bergström <andreas@textalkgroup.com>
2024-04-24 15:41:35 +0200

    changed name

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Top from "../components/Top";
+import Card from "../components/Card";
+
+const Home = () => {
+  return (
+    <>
+      <Top title={"e-wallet"} subheading={"Active Card"} />
+      <Card />
+    </>
+  );
+};
+
+export default Home;

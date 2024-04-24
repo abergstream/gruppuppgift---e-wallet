2bf77f09d3bfb80de142952f7b0e2537f78a3039
Andreas Bergström <skrap@andreasb.se>
2024-04-24 12:39:15 +0200

    Tweaking Card Component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import "./Card.css";
 
-const Card = () => {
+const Card = ({ newCard }) => {
   const cards = [
     {
       cardNumber: "1234 5678 9101 1123",
       cardName: "RACHEL RICHTER",
       validThru: "05/25",
       ccv: "666",
-      vendor: "Banknamn",
+      vendor: "bitcoin",
     },
   ];
   return (
-    <section className="card">
+    <section className={`card card--${cards[0].vendor}`}>
       <div className="card__space-between">
         <div className="card__blipp-container">
           <img className="card__image" src="gfx/blipp_dark.png" alt="" />
         </div>
-        <img className="card__image" src="gfx/bitcoin.png" alt="" />
+        <img
+          className="card__image"
+          src={`gfx/${cards[0].vendor}.png`}
+          alt=""
+        />
       </div>
       <div>
         <img className="card__image--chip" src="gfx/chip.png" alt="" />
@@ -28,7 +32,9 @@ const Card = () => {
         <p className="card__card-title">VALID THRU</p>
       </div>
       <div className="card__space-between">
-        <p className="card__card-info">{cards[0].cardName}</p>
+        <p className="card__card-info">
+          {newCard ? "NYTT NAMN" : cards[0].cardName}
+        </p>
         <p className="card__card-info">{cards[0].validThru}</p>
       </div>
     </section>

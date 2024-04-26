4c434c6624f05508aa843db4cdcb271465169f92
Vendelin2up <amy_pamy90@hotmail.com>
2024-04-26 13:14:59 +0200

    added localstorage add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -11,40 +11,8 @@ import { act } from "react-dom/test-utils";
 // npm install react-router-dom
 
 function App() {
-  const [cards, setCards] = useState([
-    {
-      cardID: 101,
-      cardNumber: "1234567891011123",
-      cardName: "RACHEL RICHTER",
-      validThru: "05/25",
-      ccv: "666",
-      vendor: "chain",
-    },
-    {
-      cardID: 102,
-      cardNumber: "9876543210987654",
-      cardName: "JOHN DOE",
-      validThru: "10/26",
-      ccv: "777",
-      vendor: "bitcoin",
-    },
-    {
-      cardID: 103,
-      cardNumber: "5555666677778888",
-      cardName: "EMMA SMITH",
-      validThru: "03/24",
-      ccv: "888",
-      vendor: "evil",
-    },
-    {
-      cardID: 104,
-      cardNumber: "4444333322221111",
-      cardName: "MICHAEL JOHNSON",
-      validThru: "08/27",
-      ccv: "999",
-      vendor: "ninja",
-    },
-  ]);
+  const fetchCards = JSON.parse(localStorage.getItem("cards"));
+  const [cards, setCards] = useState(fetchCards ? fetchCards : "");
   const [activeCard, setActiveCard] = useState(
     cards.length > 0 ? cards[cards.length - 1] : ""
   );
